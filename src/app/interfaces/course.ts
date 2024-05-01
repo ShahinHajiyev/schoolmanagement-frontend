df423c8fa4eafd3a703e20e0c52018b3a755d2e4
ShahinHajiyev <hshahin.t401@code.edu.az>
2024-05-01 10:32:04 +0200

    Course CSS added. Semester added, aligned with course.

--- src/app/interfaces/course.ts
+++ src/app/interfaces/course.ts
@@ -1,10 +1,12 @@
 import { Enrollment } from "../interfaces/enrollment";
 import { Teacher } from "../interfaces/teacher";
+import { Semester } from "./semester";
 
 export interface Course {
     courseId: number;
     courseName: string;
     credit: number;
-    teacher: Teacher;
-    enrollments: Enrollment[];
+    //teacher: Teacher;
+    //enrollments: Enrollment[];
+    semester : Semester
 }
